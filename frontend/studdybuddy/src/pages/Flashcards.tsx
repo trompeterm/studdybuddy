a4bdccbf5479b808d2b7b37d220d3de3803f4ff0
trompeterm <trompeterm@msoe.edu>
2025-05-12 14:30:52 -0500

    setup for flashcards

--- frontend/studdybuddy/src/pages/Flashcards.tsx
+++ frontend/studdybuddy/src/pages/Flashcards.tsx
@@ -1,7 +1,26 @@
+import './Flashcards.css';
+
 export default function Flashcards() {
     return (
-        <div>
-            <h1>Flashcards</h1>
+        <div className="container">
+            <h1 className="main-label">I want to studdy...</h1>
+            <div className="input-container">
+                <div className="input-group">
+                    <button>Math</button>
+                </div>
+                <div className="input-group">
+                    <button>Science</button>
+                </div>
+                <div className="input-group">
+                    <button>History</button>
+                </div>
+                <div className="input-group">
+                    <button>English</button>
+                </div>
+                <div className="input-group">
+                    <button>Coding</button>
+                </div>
+            </div>
         </div>
     )
 }
