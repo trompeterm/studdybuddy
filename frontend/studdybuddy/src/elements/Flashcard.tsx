bfa4449f38f7a623d54f5cfa28f37448113afc0d
trompeterm <trompeterm@msoe.edu>
2025-05-14 20:14:11 -0500

    color change for flashcard

--- frontend/studdybuddy/src/elements/Flashcard.tsx
+++ frontend/studdybuddy/src/elements/Flashcard.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
 import './Flashcard.css';
 
 export default function Flashcard() {
+    const [flipped, setFlipped] = useState(true);
+
     return (
         <div className="content-container">
             <div className="info-container">
@@ -10,12 +13,11 @@ export default function Flashcard() {
                     <li><button>Star</button></li>
                 </ul>
             </div>
-            <div className="card-container">
-                <div className="front">
-                    <p>Question</p>
-                </div>
-                <div className="back">
-                    <p>Answer</p>
+            <div className="card-container" onClick={() => setFlipped(!flipped)}>
+                {/* Front of card */}
+                <div className={flipped ? "front" : "back"}>
+                    <p className="question-text">This is the question</p>
+                    <p className="answer-text">This is the answer</p>
                 </div>
             </div>
             <div className="button-container">
