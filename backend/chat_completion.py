a86da33272eb0890301468b46154c11499490233
trompeterm <trompeterm@msoe.edu>
2025-05-16 17:10:10 -0500

    Created chat completion

---
+++ backend/chat_completion.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+response =client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of the moon? Be as creative as possible."}
+    ]
+)
+
+print(response.choices[0].message.content)
