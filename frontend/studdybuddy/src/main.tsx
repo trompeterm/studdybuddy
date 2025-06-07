da9806a6fd5a4989b76153efb43fb8a96f6d49b4
trompeterm <trompeterm@msoe.edu>
2025-06-07 17:49:58 -0500

    auth0 setup

--- frontend/studdybuddy/src/main.tsx
+++ frontend/studdybuddy/src/main.tsx
@@ -3,11 +3,21 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import React from 'react'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
+  <Auth0Provider
+    domain="dev-256565656565656565656565.us.auth0.com"
+    clientId="256565656565656565656565"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  </Auth0Provider>,
 )
