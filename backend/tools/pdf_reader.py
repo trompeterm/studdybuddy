e27ec8e9d3161987776a5a58be818c89d288ec07
trompeterm <trompeterm@msoe.edu>
2025-07-03 13:32:55 -0500

    pdf reader class

--- backend/tools/pdf_reader.py
+++ backend/tools/pdf_reader.py
@@ -1,6 +1,10 @@
 import fitz
 
-doc = fitz.open("../sample notes.pdf")
-for page in doc:
-    text = page.get_text()
-    print(text)
+class PDFReader:
+    def __init__(self, pdf_path):
+        self.doc = fitz.open(pdf_path)
+
+    def get_text(self):
+        for page in self.doc:
+            text = page.get_text()
+            print(text)
